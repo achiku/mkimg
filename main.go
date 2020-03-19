3eb0717036989c4a5218c6ca0f1e3d338bb454c8
Akira Chiku <akira.chiku@gmail.com>
2020-03-19 18:18:19 +0900

    Add space

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 
 	"github.com/golang/freetype"
 	"github.com/golang/freetype/truetype"
@@ -16,12 +17,13 @@ import (
 )
 
 var (
-	txt = flag.String("txt", "hello, world!", "text to image")
-	out = flag.String("outfile", "out.png", "output image path")
-	fs  = flag.Float64("fontsize", 100.0, "font size")
-	ff  = flag.String("fontfile", "", "ttf font file path")
-	w   = flag.Int("width", 1200, "image width")
-	h   = flag.Int("height", 630, "image height")
+	txt   = flag.String("txt", "hello, world!", "text to image")
+	out   = flag.String("outfile", "out.png", "output image path")
+	fs    = flag.Float64("fontsize", 100.0, "font size")
+	ff    = flag.String("fontfile", "", "ttf font file path")
+	w     = flag.Int("width", 1200, "image width")
+	h     = flag.Int("height", 630, "image height")
+	space = flag.Bool("space", false, "space image")
 )
 
 func main() {
@@ -30,8 +32,35 @@ func main() {
 	fontsize := *fs
 	width, height := *w, *h
 
-	img := image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.Draw(img, img.Bounds(), image.White, image.ZP, draw.Src)
+	var (
+		bk    draw.Image
+		img   draw.Image
+		ok    bool
+		color *image.Uniform
+	)
+	if *space {
+		sf, err := os.Open(path.Join("templates", "space2.png"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		dimg, _, err := image.Decode(sf)
+		if err != nil {
+			log.Fatalf("image.Decode failed: %s", err)
+		}
+
+		img, ok = dimg.(draw.Image)
+		if !ok {
+			log.Fatal(err)
+		}
+		bk = image.NewRGBA(image.Rect(0, 0, width, height))
+		draw.Draw(bk, img.Bounds(), img, image.ZP, draw.Src)
+		color = image.White
+	} else {
+		bk = image.NewRGBA(image.Rect(0, 0, width, height))
+		draw.Draw(bk, bk.Bounds(), image.White, image.ZP, draw.Src)
+		color = image.Black
+	}
+
 	fontBytes, err := ioutil.ReadFile(*ff)
 	if err != nil {
 		log.Fatal(err)
@@ -46,8 +75,8 @@ func main() {
 	}
 	face := truetype.NewFace(ft, &opt)
 	dr := &font.Drawer{
-		Dst:  img,
-		Src:  image.Black,
+		Dst:  bk,
+		Src:  color,
 		Face: face,
 		Dot:  fixed.Point26_6{},
 	}
@@ -64,7 +93,7 @@ func main() {
 	}
 	defer outfile.Close()
 
-	if err := png.Encode(outfile, img); err != nil {
+	if err := png.Encode(outfile, bk); err != nil {
 		log.Fatal(err)
 	}
 }
