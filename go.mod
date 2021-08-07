b41e51ddcf552438c5fcca9e0afe12c0f7413af7
Akira Chiku <akira.chiku@gmail.com>
2021-08-07 18:43:34 +0900

    Try gg

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/achiku/mkimg
 go 1.12
 
 require (
-	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
-	golang.org/x/image v0.0.0-20191214001246-9130b4cfad52
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20191214001246-9130b4cfad52 // indirect
 )
--- main.go
+++ main.go
@@ -2,98 +2,32 @@ package main
 
 import (
 	"flag"
-	"image"
-	"image/draw"
-	"image/png"
-	"io/ioutil"
-	"log"
-	"os"
-	"path"
 
-	"github.com/golang/freetype"
-	"github.com/golang/freetype/truetype"
-	"golang.org/x/image/font"
-	"golang.org/x/image/math/fixed"
+	"github.com/fogleman/gg"
 )
 
 var (
-	txt   = flag.String("txt", "hello, world!", "text to image")
-	out   = flag.String("outfile", "out.png", "output image path")
-	fs    = flag.Float64("fontsize", 100.0, "font size")
-	ff    = flag.String("fontfile", "", "ttf font file path")
-	w     = flag.Int("width", 1200, "image width")
-	h     = flag.Int("height", 630, "image height")
-	space = flag.Bool("space", false, "space image")
+	txt = flag.String("txt", "hello, world!", "text to image")
+	out = flag.String("outfile", "out.png", "output image path")
+	fs  = flag.Float64("fontsize", 100.0, "font size")
+	ff  = flag.String("fontfile", "", "ttf font file path")
+	w   = flag.Int("width", 1200, "image width")
+	h   = flag.Int("height", 630, "image height")
+	// space = flag.Bool("space", false, "space image")
 )
 
 func main() {
 	flag.Parse()
 
-	fontsize := *fs
-	width, height := *w, *h
-
-	var (
-		bk    draw.Image
-		img   draw.Image
-		ok    bool
-		color *image.Uniform
-	)
-	if *space {
-		sf, err := os.Open(path.Join("templates", "space2.png"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		dimg, _, err := image.Decode(sf)
-		if err != nil {
-			log.Fatalf("image.Decode failed: %s", err)
-		}
-
-		img, ok = dimg.(draw.Image)
-		if !ok {
-			log.Fatal(err)
-		}
-		bk = image.NewRGBA(image.Rect(0, 0, width, height))
-		draw.Draw(bk, img.Bounds(), img, image.ZP, draw.Src)
-		color = image.White
-	} else {
-		bk = image.NewRGBA(image.Rect(0, 0, width, height))
-		draw.Draw(bk, bk.Bounds(), image.White, image.ZP, draw.Src)
-		color = image.Black
+	W := 1200.0
+	H := 630.0
+	P := 16.0
+	dc := gg.NewContext(int(W), int(H))
+	dc.SetRGB(1, 1, 1)
+	dc.SetRGB(0, 0, 0)
+	if err := dc.LoadFontFace(*ff, 18); err != nil {
+		panic(err)
 	}
-
-	fontBytes, err := ioutil.ReadFile(*ff)
-	if err != nil {
-		log.Fatal(err)
-	}
-	ft, err := freetype.ParseFont(fontBytes)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	opt := truetype.Options{
-		Size: fontsize,
-	}
-	face := truetype.NewFace(ft, &opt)
-	dr := &font.Drawer{
-		Dst:  bk,
-		Src:  color,
-		Face: face,
-		Dot:  fixed.Point26_6{},
-	}
-	x := (fixed.I(width) - dr.MeasureString(*txt)) / 2
-	dr.Dot.X = x
-	y := (height + int(fontsize)/2) / 2
-	dr.Dot.Y = fixed.I(y)
-
-	dr.DrawString(*txt)
-
-	outfile, err := os.Create(*out)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer outfile.Close()
-
-	if err := png.Encode(outfile, bk); err != nil {
-		log.Fatal(err)
-	}
+	dc.DrawStringWrapped(*txt, W/2-P, H/2-P, 1, 1, W, 1.75, gg.AlignCenter)
+	dc.SavePNG(*out)
 }
