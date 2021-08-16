682259adde310861c6a2820afccd4493a377030a
Akira Chiku <akira.chiku@gmail.com>
2021-08-16 13:46:53 +0900

    Update

--- mkimg.go
+++ mkimg.go
@@ -6,12 +6,12 @@ import (
 	"image/draw"
 
 	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 )
 
 func drawStringDebug(s string, d *font.Drawer) {
 	prevC := rune(-1)
 	for _, c := range s {
-		fmt.Printf("%#U\n", c)
 		if prevC >= 0 {
 			d.Dot.X += d.Face.Kern(prevC, c)
 		}
@@ -24,9 +24,89 @@ func drawStringDebug(s string, d *font.Drawer) {
 		}
 		draw.DrawMask(d.Dst, dr, d.Src, image.Point{}, mask, maskp, draw.Over)
 		d.Dot.X += advance
-		fmt.Printf("maskp=%v, advance=%d, X=%d\n", maskp, advance, d.Dot.X)
+		fmt.Printf("%#U: maskp=%v, advance=%d, X=%d, Y=%d\n", c, maskp, advance, d.Dot.X, d.Dot.Y)
 		prevC = c
 	}
+}
+
+// DrawStringOpts options
+type DrawStringOpts struct {
+	ImageWidth       fixed.Int26_6
+	ImageHeight      fixed.Int26_6
+	VerticalMargin   fixed.Int26_6
+	HorizontalMargin fixed.Int26_6
+	FontSize         fixed.Int26_6
+	LineSpace        fixed.Int26_6
+	Verbose          bool
+}
+
+// DrawStringWrapped draw string wrapped
+func DrawStringWrapped(d *font.Drawer, s string, opt *DrawStringOpts) {
+	d.Dot.X = opt.HorizontalMargin
+	d.Dot.Y = opt.FontSize + opt.VerticalMargin
+	// originalX := d.Dot.X
+	originalY := d.Dot.Y
+	prevC := rune(-1)
+	for _, c := range s {
+		if prevC >= 0 {
+			d.Dot.X += d.Face.Kern(prevC, c)
+		}
+		dr, mask, maskp, advance, ok := d.Face.Glyph(d.Dot, c)
+		if !ok {
+			// TODO: is falling back on the U+FFFD glyph the responsibility of
+			// the Drawer or the Face?
+			// TODO: set prevC = '\ufffd'?
+			continue
+		}
+		if d.Dot.X+advance >= (opt.ImageWidth - opt.HorizontalMargin*2) {
+			d.Dot.Y = originalY + d.Dot.Y + opt.LineSpace
+			d.Dot.X = fixed.I(0)
+		}
+
+		if opt.Verbose {
+			fmt.Printf(
+				"%#U: maskp=%+v, advance=%d, X=%d, w=%d, Y=%d, h=%d, realW=%d\n",
+				c, maskp, advance, d.Dot.X, opt.ImageWidth, d.Dot.Y, opt.ImageHeight, (opt.ImageWidth - opt.HorizontalMargin*2))
+		}
+		draw.DrawMask(d.Dst, dr, d.Src, image.Point{}, mask, maskp, draw.Over)
+		d.Dot.X += advance
+		prevC = c
+	}
+}
+
+// MeasureString returns how far dot would advance by drawing s with f.
+func MeasureString(f font.Face, s string) (advance fixed.Int26_6) {
+	prevC := rune(-1)
+	for _, c := range s {
+		if prevC >= 0 {
+			advance += f.Kern(prevC, c)
+		}
+		a, ok := f.GlyphAdvance(c)
+		if !ok {
+			// TODO: is falling back on the U+FFFD glyph the responsibility of
+			// the Drawer or the Face?
+			// TODO: set prevC = '\ufffd'?
+			continue
+		}
+		advance += a
+		prevC = c
+	}
+	return advance
+}
+
+// Text represents text input
+type Text struct {
+	OriginalText string
+	Lines        []TextLine
+	Padding      int
+	Width        int
+}
+
+// TextLine text line
+type TextLine struct {
+	StartingX int
+	StartingY int
+	Words     string
 }
 
 func calculatePoints(s string, w, h int) (int, int) {
--- mkimg_test.go
+++ mkimg_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image"
+	"image/color"
 	"image/draw"
 	"image/png"
 	"io/ioutil"
@@ -16,11 +17,29 @@ import (
 	"golang.org/x/image/math/fixed"
 )
 
+func TestDraw(t *testing.T) {
+	m := image.NewRGBA(image.Rect(0, 0, 640, 480))
+	blue := color.RGBA{0, 0, 255, 255}
+
+	draw.Draw(m, m.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
+	outfile, err := os.Create("out.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
+
+	if err := png.Encode(outfile, m); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
 func TestDrawString(t *testing.T) {
 	width, height := 1200, 630
 	fontsize := 10.0
 	txt := "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。"
 	// txt := "それは本当にそう"
+	// txt := "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off—then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me."
 	ff := "./Koruri-Bold.ttf"
 	bk := image.NewRGBA(image.Rect(0, 0, width, height))
 	draw.Draw(bk, bk.Bounds(), image.White, image.ZP, draw.Src)
@@ -45,7 +64,7 @@ func TestDrawString(t *testing.T) {
 		Face: face,
 		Dot:  fixed.Point26_6{},
 	}
-	fmt.Printf("dr.MeasureString(txt)=%d\n", dr.MeasureString(txt))
+	fmt.Printf("dr.MeasureString(txt)=%d\n", MeasureString(dr.Face, txt))
 	fmt.Printf("fixed.I(width)=%d\n", fixed.I(width))
 	fmt.Printf("shift width=%d\n", int64(width<<6))
 	//x := (fixed.I(width) - dr.MeasureString(txt)) / 2
@@ -54,6 +73,60 @@ func TestDrawString(t *testing.T) {
 	dr.Dot.Y = fixed.I(y)
 
 	drawStringDebug(txt, dr)
+
+	outfile, err := os.Create("out.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer outfile.Close()
+
+	if err := png.Encode(outfile, bk); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestDrawStringWrapped(t *testing.T) {
+	width, height := 1200, 630
+	fontsize := 50.0
+	txt := "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。"
+	bk := image.NewRGBA(image.Rect(0, 0, width, height))
+	draw.Draw(bk, bk.Bounds(), image.White, image.ZP, draw.Src)
+
+	ff := "./Koruri-Bold.ttf"
+	fontBytes, err := ioutil.ReadFile(ff)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ft, err := freetype.ParseFont(fontBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	opt := truetype.Options{
+		Size: fontsize,
+	}
+	face := truetype.NewFace(ft, &opt)
+	dr := &font.Drawer{
+		Dst:  bk,
+		Src:  image.Black,
+		Face: face,
+		Dot:  fixed.Point26_6{},
+	}
+	fmt.Printf("dr.MeasureString(txt)=%d\n", MeasureString(dr.Face, txt))
+	fmt.Printf("fixed.I(width)=%d\n", fixed.I(width))
+	fmt.Printf("shift width=%d\n", int64(width<<6))
+
+	dOpt := &DrawStringOpts{
+		ImageWidth:       fixed.I(width),
+		ImageHeight:      fixed.I(height),
+		Verbose:          true,
+		FontSize:         fixed.I(int(fontsize)),
+		LineSpace:        fixed.I(5),
+		VerticalMargin:   fixed.I(10),
+		HorizontalMargin: fixed.I(80),
+	}
+
+	DrawStringWrapped(dr, txt, dOpt)
 
 	outfile, err := os.Create("out.png")
 	if err != nil {
