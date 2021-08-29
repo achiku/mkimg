ef20d0b3350d1255d1d4d57343e599afa0018f01
Akira Chiku <akira.chiku@gmail.com>
2021-08-29 12:54:05 +0900

    Add DrawStringWrap

--- mkimg.go
+++ mkimg.go
@@ -9,26 +9,6 @@ import (
 	"golang.org/x/image/math/fixed"
 )
 
-func drawStringDebug(s string, d *font.Drawer) {
-	prevC := rune(-1)
-	for _, c := range s {
-		if prevC >= 0 {
-			d.Dot.X += d.Face.Kern(prevC, c)
-		}
-		dr, mask, maskp, advance, ok := d.Face.Glyph(d.Dot, c)
-		if !ok {
-			// TODO: is falling back on the U+FFFD glyph the responsibility of
-			// the Drawer or the Face?
-			// TODO: set prevC = '\ufffd'?
-			continue
-		}
-		draw.DrawMask(d.Dst, dr, d.Src, image.Point{}, mask, maskp, draw.Over)
-		d.Dot.X += advance
-		fmt.Printf("%#U: maskp=%v, advance=%d, X=%d, Y=%d\n", c, maskp, advance, d.Dot.X, d.Dot.Y)
-		prevC = c
-	}
-}
-
 // DrawStringOpts options
 type DrawStringOpts struct {
 	ImageWidth       fixed.Int26_6
@@ -44,14 +24,15 @@ type DrawStringOpts struct {
 func DrawStringWrapped(d *font.Drawer, s string, opt *DrawStringOpts) {
 	d.Dot.X = opt.HorizontalMargin
 	d.Dot.Y = opt.FontSize + opt.VerticalMargin
-	// originalX := d.Dot.X
-	originalY := d.Dot.Y
+	originalX, originalY := d.Dot.X, d.Dot.Y
+
 	prevC := rune(-1)
 	for _, c := range s {
 		if prevC >= 0 {
 			d.Dot.X += d.Face.Kern(prevC, c)
 		}
-		dr, mask, maskp, advance, ok := d.Face.Glyph(d.Dot, c)
+		// dr, mask, maskp, advance, ok := d.Face.Glyph(d.Dot, c)
+		advance, ok := d.Face.GlyphAdvance(c)
 		if !ok {
 			// TODO: is falling back on the U+FFFD glyph the responsibility of
 			// the Drawer or the Face?
@@ -59,10 +40,13 @@ func DrawStringWrapped(d *font.Drawer, s string, opt *DrawStringOpts) {
 			continue
 		}
 		if d.Dot.X+advance >= (opt.ImageWidth - opt.HorizontalMargin*2) {
+			if opt.Verbose {
+				fmt.Printf("### new line: %#U, x=%d, y=%d, ", c, d.Dot.X, d.Dot.Y)
+			}
 			d.Dot.Y = originalY + d.Dot.Y + opt.LineSpace
-			d.Dot.X = fixed.I(0)
+			d.Dot.X = originalX
 		}
-
+		dr, mask, maskp, advance, ok := d.Face.Glyph(d.Dot, c)
 		if opt.Verbose {
 			fmt.Printf(
 				"%#U: maskp=%+v, advance=%d, X=%d, w=%d, Y=%d, h=%d, realW=%d\n",
@@ -92,23 +76,4 @@ func MeasureString(f font.Face, s string) (advance fixed.Int26_6) {
 		prevC = c
 	}
 	return advance
-}
-
-// Text represents text input
-type Text struct {
-	OriginalText string
-	Lines        []TextLine
-	Padding      int
-	Width        int
-}
-
-// TextLine text line
-type TextLine struct {
-	StartingX int
-	StartingY int
-	Words     string
-}
-
-func calculatePoints(s string, w, h int) (int, int) {
-	return 0, 0
 }
--- mkimg_test.go
+++ mkimg_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
@@ -34,61 +33,11 @@ func TestDraw(t *testing.T) {
 
 }
 
-func TestDrawString(t *testing.T) {
-	width, height := 1200, 630
-	fontsize := 10.0
-	txt := "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。"
-	// txt := "それは本当にそう"
-	// txt := "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people's hats off—then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me."
-	ff := "./Koruri-Bold.ttf"
-	bk := image.NewRGBA(image.Rect(0, 0, width, height))
-	draw.Draw(bk, bk.Bounds(), image.White, image.ZP, draw.Src)
-	color := image.Black
-
-	fontBytes, err := ioutil.ReadFile(ff)
-	if err != nil {
-		t.Fatal(err)
-	}
-	ft, err := freetype.ParseFont(fontBytes)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	opt := truetype.Options{
-		Size: fontsize,
-	}
-	face := truetype.NewFace(ft, &opt)
-	dr := &font.Drawer{
-		Dst:  bk,
-		Src:  color,
-		Face: face,
-		Dot:  fixed.Point26_6{},
-	}
-	fmt.Printf("dr.MeasureString(txt)=%d\n", MeasureString(dr.Face, txt))
-	fmt.Printf("fixed.I(width)=%d\n", fixed.I(width))
-	fmt.Printf("shift width=%d\n", int64(width<<6))
-	//x := (fixed.I(width) - dr.MeasureString(txt)) / 2
-	//dr.Dot.X = x
-	y := (height + int(fontsize)/2) / 2
-	dr.Dot.Y = fixed.I(y)
-
-	drawStringDebug(txt, dr)
-
-	outfile, err := os.Create("out.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer outfile.Close()
-
-	if err := png.Encode(outfile, bk); err != nil {
-		log.Fatal(err)
-	}
-}
-
 func TestDrawStringWrapped(t *testing.T) {
 	width, height := 1200, 630
-	fontsize := 50.0
-	txt := "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。"
+	fontsize := 20.0
+	// txt := "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。"
+	txt := "今やろうとしている事は「巨大な価値移動API(≒Visa)を使って現代の人が持つ痛みを解決するソフトウェアカンパニーを作る事」で、その為には消費財側の人たちが蓄積してきた知見が必要なんすよね。最強のチームを作ってブチ抜くぞ。金融実務の知識はその一部であって全てではない。自分たちはあくまでもソフトウェカンパニーであり、そのソフトウェアは現代に生きる人達が持っているまだ言語化されていない課題を解決するもの、新しいカテゴリーを切り開くもの。だから色んな領域の方の知識と経験が必要なんです。"
 	bk := image.NewRGBA(image.Rect(0, 0, width, height))
 	draw.Draw(bk, bk.Bounds(), image.White, image.ZP, draw.Src)
 
@@ -112,9 +61,9 @@ func TestDrawStringWrapped(t *testing.T) {
 		Face: face,
 		Dot:  fixed.Point26_6{},
 	}
-	fmt.Printf("dr.MeasureString(txt)=%d\n", MeasureString(dr.Face, txt))
-	fmt.Printf("fixed.I(width)=%d\n", fixed.I(width))
-	fmt.Printf("shift width=%d\n", int64(width<<6))
+	t.Logf("dr.MeasureString(txt)=%d\n", MeasureString(dr.Face, txt))
+	t.Logf("fixed.I(width)=%d\n", fixed.I(width))
+	t.Logf("shift width=%d\n", int64(width<<6))
 
 	dOpt := &DrawStringOpts{
 		ImageWidth:       fixed.I(width),
@@ -123,7 +72,7 @@ func TestDrawStringWrapped(t *testing.T) {
 		FontSize:         fixed.I(int(fontsize)),
 		LineSpace:        fixed.I(5),
 		VerticalMargin:   fixed.I(10),
-		HorizontalMargin: fixed.I(80),
+		HorizontalMargin: fixed.I(40),
 	}
 
 	DrawStringWrapped(dr, txt, dOpt)
